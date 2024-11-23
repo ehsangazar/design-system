a29d8ced522fe8c7afd4dc99473aade1848c9e6b
Gaz <gaz.ehsan@tipalti.com>
2024-11-23 23:29:40 +0000

    refactor Text component stories; enhance argTypes for better control and add multiple rendering scenarios

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,24 +1,112 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Text from "./Text";
 
-export default {
-  title: "Components/Text",
+const meta: Meta<typeof Text> = {
+  title: "Typography/Text",
   component: Text,
-} as ComponentMeta<typeof Text>;
+  argTypes: {
+    children: {
+      control: "text",
+    },
+    as: {
+      control: "select",
+      options: ["span", "div", "label", "p"],
+    },
+    size: {
+      control: "select",
+      options: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+    },
+    weight: {
+      control: "select",
+      options: ["light", "regular", "medium", "bold"],
+    },
+    align: {
+      control: "select",
+      options: ["left", "center", "right"],
+    },
+    trim: {
+      control: "select",
+      options: ["normal", "start", "end", "both"],
+    },
+    truncate: {
+      control: "boolean",
+    },
+    wrap: {
+      control: "select",
+      options: ["wrap", "nowrap", "pretty", "balance"],
+    },
+    color: {
+      control: "select",
+      options: [
+        "gray",
+        "gold",
+        "bronze",
+        "brown",
+        "yellow",
+        "amber",
+        "orange",
+        "tomato",
+        "red",
+        "ruby",
+        "crimson",
+        "pink",
+        "plum",
+        "purple",
+        "violet",
+        "iris",
+        "indigo",
+        "blue",
+        "cyan",
+        "teal",
+        "jade",
+        "green",
+        "grass",
+        "lime",
+        "mint",
+        "sky",
+      ],
+    },
+    highContrast: {
+      control: "boolean",
+    },
+  },
+};
 
-const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+export default meta;
+type Story = StoryObj<typeof Text>;
 
-export const AllScenarios = Template.bind({});
-AllScenarios.args = {
-  as: "p",
-  asChild: false,
-  size: { base: "medium", md: "large" },
-  weight: { base: "regular", md: "bold" },
-  align: { base: "left", md: "center" },
-  trim: { base: "normal", md: "both" },
-  truncate: false,
-  wrap: { base: "wrap", md: "nowrap" },
-  color: "primary",
-  highContrast: true,
+export const Default: Story = {
+  args: {
+    children:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    size: "3",
+  },
+};
+
+export const Span: Story = {
+  args: {
+    children:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    as: "span",
+    size: "3",
+  },
+};
+
+export const Div: Story = {
+  args: {
+    children:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    as: "div",
+    size: "3",
+  },
+};
+
+export const Label: Story = {
+  args: {
+    children:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    as: "label",
+    size: "2",
+    weight: "bold",
+  },
 };
