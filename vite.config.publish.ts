c8022c4b4034d9e35e2e4ae8c0fd9e391e95d637
Gaz <gaz.ehsan@tipalti.com>
2024-11-30 18:28:25 +0000

    feat: add Vite configuration for publishing and update package.json for build scripts

---
+++ vite.config.publish.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: "src/index.ts",
+      name: "Gaz Design System",
+      fileName: (format) => `gaz-design-system.${format}.js`,
+      formats: ["es", "cjs", "umd", "iife"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
