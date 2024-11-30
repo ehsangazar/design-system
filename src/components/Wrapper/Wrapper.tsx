872bbf0657d1fef0c16c73cda937f716b5a5a4a6
Gaz <gaz.ehsan@tipalti.com>
2024-11-30 16:58:52 +0000

    feat: enhance Theme interface with new properties for appearance, scaling, and event handlers

--- src/components/Wrapper/Wrapper.tsx
+++ src/components/Wrapper/Wrapper.tsx
@@ -7,6 +7,12 @@ import "@radix-ui/themes/layout.css";
 import "./main.css";
 
 interface Theme {
+  apprearance: "light" | "dark";
+  onAccentColorChange: (accentColor: Theme["accentColor"]) => void;
+  onGrayColorChange: (grayColor: Theme["grayColor"]) => void;
+  onPanelBackgroundChange: (panelBackground: ["solid", "translucent"]) => void;
+  onRadiusChange: (radius: Theme["radius"]) => void;
+  onScalingChange: (scaling: ["90%", "95%", "100%", "105%", "110%"]) => void;
   accentColor:
     | "gray"
     | "gold"
@@ -52,14 +58,33 @@ interface Theme {
     | "jade"
     | "green"
     | "grass";
-  grayColor: "sand" | "slate";
+  grayColor: "auto" | "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
+  resolvedGrayColor: "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
+  panelBackground: "solid" | "translucent";
   radius: "small" | "medium" | "large" | "none" | "full";
+  scaling: "90%" | "95%" | "100%" | "105%" | "110%";
+  breakpoints: { [key: string]: string };
 }
 
 const defaultTheme: Theme = {
+  apprearance: "light",
+  onAccentColorChange: () => {},
+  onGrayColorChange: () => {},
+  onPanelBackgroundChange: () => {},
+  onRadiusChange: () => {},
+  onScalingChange: () => {},
   accentColor: "cyan",
   grayColor: "sand",
+  resolvedGrayColor: "sand",
+  panelBackground: "solid",
   radius: "medium",
+  scaling: "100%",
+  breakpoints: {
+    sm: "640px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1280px",
+  },
 };
 
 const Wrapper = ({
