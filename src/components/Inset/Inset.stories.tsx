5a7b9f68b795e58cf66d93910200443813a66e86
Gaz <gaz.ehsan@tipalti.com>
2024-12-02 00:18:15 +0000

    feat: add vite-plugin-css dependency and update Inset story props

--- src/components/Inset/Inset.stories.tsx
+++ src/components/Inset/Inset.stories.tsx
@@ -46,15 +46,15 @@ type Story = StoryObj<typeof Inset>;
 
 export const Default: Story = {
   args: {
-    side: "all",
-    clip: "border-box",
-    p: "0",
+    side: "top",
+    clip: "padding-box",
+    pb: "current",
     children: "Inset",
   },
-  render: () => (
+  render: (props) => (
     <Box maxWidth="240px">
       <Card size="2">
-        <Inset clip="padding-box" side="top" pb="current">
+        <Inset {...props}>
           <img
             src="https://images.unsplash.com/photo-1617050318658-a9a3175e34cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=600&q=80"
             alt="Bold typography"
