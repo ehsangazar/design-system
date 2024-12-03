f0f690ee100d96bb946da498f159757e32945258
Gaz <gaz.ehsan@tipalti.com>
2024-12-03 23:53:08 +0000

    feat: enhance Button component with additional padding and margin props for improved layout control

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,6 +7,20 @@ export interface ButtonProps extends RadixButtonProps {
   width?: string;
   height?: string;
   m?: string;
+  p: string;
+  mt?: string;
+  mr?: string;
+  mb?: string;
+  ml?: string;
+  mx?: string;
+  my?: string;
+  pt?: string;
+  pr?: string;
+  pb?: string;
+  pl?: string;
+  px?: string;
+  py?: string;
+  padding?: string;
   margin?: string;
   colorScheme?: string;
   isActive?: boolean;
@@ -21,6 +35,7 @@ const Button = ({ children, ...rest }: ButtonProps) => {
   // Custom style
   if (rest.colorScheme) {
     customStyle.color = rest.colorScheme;
+    delete customProps.colorScheme;
   }
   if (rest.w || rest.width) {
     customStyle.width = rest.w || rest.width;
@@ -30,6 +45,49 @@ const Button = ({ children, ...rest }: ButtonProps) => {
   }
   if (rest.m || rest.margin) {
     customStyle.margin = rest.m || rest.margin;
+  }
+  if (rest.p || rest.padding) {
+    customStyle.padding = rest.p || rest.padding;
+  }
+  if (rest.mt) {
+    customStyle.marginTop = rest.mt;
+  }
+  if (rest.mr) {
+    customStyle.marginRight = rest.mr;
+  }
+  if (rest.mb) {
+    customStyle.marginBottom = rest.mb;
+  }
+  if (rest.ml) {
+    customStyle.marginLeft = rest.ml;
+  }
+  if (rest.mx) {
+    customStyle.marginLeft = rest.mx;
+    customStyle.marginRight = rest.mx;
+  }
+  if (rest.my) {
+    customStyle.marginTop = rest.my;
+    customStyle.marginBottom = rest.my;
+  }
+  if (rest.pt) {
+    customStyle.paddingTop = rest.pt;
+  }
+  if (rest.pr) {
+    customStyle.paddingRight = rest.pr;
+  }
+  if (rest.pb) {
+    customStyle.paddingBottom = rest.pb;
+  }
+  if (rest.pl) {
+    customStyle.paddingLeft = rest.pl;
+  }
+  if (rest.px) {
+    customStyle.paddingLeft = rest.px;
+    customStyle.paddingRight = rest.px;
+  }
+  if (rest.py) {
+    customStyle.paddingTop = rest.py;
+    customStyle.paddingBottom = rest.py;
   }
 
   // Custom props
