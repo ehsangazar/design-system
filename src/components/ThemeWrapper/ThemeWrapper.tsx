fbf4ea67e954cd9a4ce1875b80c799c4f7e7d374
Gaz <gaz.ehsan@tipalti.com>
2024-12-04 02:03:46 +0000

    feat: refactor Theme interface to use optional properties and improve type definitions

--- src/components/ThemeWrapper/ThemeWrapper.tsx
+++ src/components/ThemeWrapper/ThemeWrapper.tsx
@@ -8,6 +8,45 @@ import "./theme.css";
 import "./main.css";
 import { generateShades } from "./generateShades";
 
+interface Theme {
+  apprearance?: "light" | "dark";
+  onAccentColorChange?: (accentColor: Theme["accentColor"]) => void;
+  onGrayColorChange?: (grayColor: Theme["grayColor"]) => void;
+  onPanelBackgroundChange?: (panelBackground: ["solid", "translucent"]) => void;
+  onRadiusChange?: (radius: Theme["radius"]) => void;
+  onScalingChange?: (scaling: ["90%", "95%", "100%", "105%", "110%"]) => void;
+  accentColor?:
+    | "gray"
+    | "gold"
+    | "bronze"
+    | "brown"
+    | "yellow"
+    | "amber"
+    | "orange"
+    | "tomato"
+    | "red"
+    | "ruby"
+    | "crimson"
+    | "pink"
+    | "plum"
+    | "purple"
+    | "violet"
+    | "iris"
+    | "indigo"
+    | "blue"
+    | "cyan"
+    | "teal"
+    | "jade"
+    | "green"
+    | "grass";
+  grayColor?: "auto" | "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
+  resolvedGrayColor?: "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
+  panelBackground?: "solid" | "translucent";
+  radius?: "small" | "medium" | "large" | "none" | "full";
+  scaling?: "90%" | "95%" | "100%" | "105%" | "110%";
+  breakpoints?: { [key: string]: string };
+}
+
 const defaultTheme: Theme = {
   apprearance: "light",
   onAccentColorChange: () => {},
@@ -59,45 +98,6 @@ const defaultColors = {
   slate: "#8B8D98",
   mauve: "#8E8C99",
 };
-
-interface Theme {
-  apprearance: "light" | "dark";
-  onAccentColorChange: (accentColor: Theme["accentColor"]) => void;
-  onGrayColorChange: (grayColor: Theme["grayColor"]) => void;
-  onPanelBackgroundChange: (panelBackground: ["solid", "translucent"]) => void;
-  onRadiusChange: (radius: Theme["radius"]) => void;
-  onScalingChange: (scaling: ["90%", "95%", "100%", "105%", "110%"]) => void;
-  accentColor:
-    | "gray"
-    | "gold"
-    | "bronze"
-    | "brown"
-    | "yellow"
-    | "amber"
-    | "orange"
-    | "tomato"
-    | "red"
-    | "ruby"
-    | "crimson"
-    | "pink"
-    | "plum"
-    | "purple"
-    | "violet"
-    | "iris"
-    | "indigo"
-    | "blue"
-    | "cyan"
-    | "teal"
-    | "jade"
-    | "green"
-    | "grass";
-  grayColor: "auto" | "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
-  resolvedGrayColor: "gray" | "mauve" | "slate" | "sage" | "olive" | "sand";
-  panelBackground: "solid" | "translucent";
-  radius: "small" | "medium" | "large" | "none" | "full";
-  scaling: "90%" | "95%" | "100%" | "105%" | "110%";
-  breakpoints: { [key: string]: string };
-}
 
 const ThemeWrapper = ({
   children,
