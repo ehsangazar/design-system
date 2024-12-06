b4a118aedee9c2fc442bc0f9112f5d0674b9809c
Gaz <gaz.ehsan@tipalti.com>
2024-12-06 23:53:08 +0000

    feat: add GazLink component with customizable styles and hover effects; update GazCard hover styles

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,5 +1,27 @@
 import { Link as RadixLink } from "@radix-ui/themes";
+import type { LinkProps as RadixLinkProps } from "@radix-ui/themes";
+import "./Link.css";
 
-const Link = RadixLink;
+const Link = ({ children, ...rest }: RadixLinkProps) => {
+  rest.style = {
+    cursor: "pointer",
+    textDecoration: rest?.underline ? "underline" : "none",
+    textDecorationThickness: "3px",
+    ...rest.style,
+  };
+
+  let className = `GazLink`;
+  className += ` GazLink--${rest.color}`;
+
+  if (rest.className) {
+    className += ` ${rest.className}`;
+  }
+
+  return (
+    <RadixLink className={className} {...rest}>
+      {children}
+    </RadixLink>
+  );
+};
 
 export default Link;
