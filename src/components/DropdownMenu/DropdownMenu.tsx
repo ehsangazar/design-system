7856b3a1fede887fe83fa57b27706bb59a755703
Gaz <gaz.ehsan@tipalti.com>
2024-12-07 02:47:05 +0000

    feat: remove Select and DropdownMenu components; clean up unused dependencies

--- src/components/DropdownMenu/DropdownMenu.tsx
+++
@@ -1,5 +0,0 @@
-import { DropdownMenu as RadixDropdownMenu } from "@radix-ui/themes";
-
-const DropdownMenu = RadixDropdownMenu;
-
-export default DropdownMenu;
--- src/components/Select/Select.tsx
+++
@@ -1,5 +0,0 @@
-import { Select as RadixSelect } from "@radix-ui/themes";
-
-const Select = RadixSelect;
-
-export default Select;
