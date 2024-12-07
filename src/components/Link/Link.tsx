fcbc7399e58ef6809b96c34bca221df8bacb0ea5
Gaz <gaz.ehsan@tipalti.com>
2024-12-07 00:06:40 +0000

    feat: update GazCard styles and enhance GazLink component with conditional underline classes

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -3,15 +3,16 @@ import type { LinkProps as RadixLinkProps } from "@radix-ui/themes";
 import "./Link.css";
 
 const Link = ({ children, ...rest }: RadixLinkProps) => {
-  rest.style = {
-    cursor: "pointer",
-    textDecoration: rest?.underline ? "underline" : "none",
-    textDecorationThickness: "3px",
-    ...rest.style,
-  };
-
   let className = `GazLink`;
   className += ` GazLink--${rest.color}`;
+
+  if (rest.underline === "always") {
+    className += ` GazLink--underline`;
+  }
+
+  if (rest.underline === "hover") {
+    className += ` GazLink--underlineHover`;
+  }
 
   if (rest.className) {
     className += ` ${rest.className}`;
