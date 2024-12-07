a380db3a126a7bba3adaf9ff7a8aa2fc9d9e98fc
Gaz <gaz.ehsan@tipalti.com>
2024-12-07 20:29:31 +0000

    feat: enhance InputTextArea component with forwardRef support

--- src/components/InputTextArea/InputTextArea.tsx
+++ src/components/InputTextArea/InputTextArea.tsx
@@ -1,5 +1,11 @@
 import { TextArea as RadixInputTextArea } from "@radix-ui/themes";
+import React from "react";
 
-const InputTextArea = RadixInputTextArea;
+const InputTextArea = React.forwardRef<
+  HTMLTextAreaElement,
+  React.ComponentProps<typeof RadixInputTextArea>
+>((props, ref) => {
+  return <RadixInputTextArea ref={ref} {...props} />;
+});
 
 export default InputTextArea;
