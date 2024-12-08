e38ec658c8d431d4257398e916de0d7912f0d6b1
Gaz <gaz.ehsan@tipalti.com>
2024-12-08 02:06:04 +0000

    feat: enhance InputDropdown styles and add theme support in Storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,12 +3,34 @@ import type { Preview } from "@storybook/react";
 import ThemeWrapper from "../src/components/ThemeWrapper/ThemeWrapper";
 import "./styles.css";
 
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    description: "Global theme for components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "circlehollow",
+      items: [
+        { value: "light", title: "Light Mode" },
+        { value: "dark", title: "Dark Mode" },
+      ],
+    },
+  },
+};
+
 export const decorators = [
-  (Story) => (
-    <ThemeWrapper>
-      <Story />
-    </ThemeWrapper>
-  ),
+  (Story, context) => {
+    const theme = context.globals.theme;
+    return (
+      <ThemeWrapper
+        customTheme={{
+          appearance: theme,
+        }}
+      >
+        <Story />
+      </ThemeWrapper>
+    );
+  },
 ];
 
 const preview: Preview = {
--- src/components/InputDropdown/InputDropdown.tsx
+++ src/components/InputDropdown/InputDropdown.tsx
@@ -12,14 +12,36 @@ const InputDropdown = React.forwardRef<any, SelectProps>((props, ref) => {
         control: (styles) => {
           return {
             ...styles,
+            background: "transparent",
             fontSize: "14px",
             borderColor: "var(--gray-6)",
+            color: "var(--gray-12)",
+          };
+        },
+        menu: (styles) => {
+          return {
+            ...styles,
+            background: "var(--color-panel-solid)",
+            color: "var(--gray-12)",
+          };
+        },
+        input: (styles) => {
+          return {
+            ...styles,
+            color: "var(--gray-12)",
           };
         },
         option: (styles) => {
           return {
             ...styles,
             fontSize: "14px",
+            color: "var(--gray-12)",
+          };
+        },
+        singleValue: (styles) => {
+          return {
+            ...styles,
+            color: "var(--gray-12)",
           };
         },
       }}
