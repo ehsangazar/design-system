a577d09233127c5138c976a8fca3d7204b0bb64e
Gaz <gaz.ehsan@tipalti.com>
2024-12-22 01:59:21 +0000

    fix: reduce sleep duration for Storybook startup and add global test function for Jest

--- test-runner-jest.config.js
+++ test-runner-jest.config.js
@@ -1,6 +1,7 @@
 import { getJestConfig } from "@storybook/test-runner";
 
 const testRunnerConfig = getJestConfig();
+global.__test = (name, fn) => test(name, fn);
 
 export default {
   ...testRunnerConfig,
